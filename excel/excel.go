1a6fca81b864114b8945a221a7a06e60839baec7
Jericho Yu <yu.jericho.ph@gmail.com>
2024-06-09 18:04:33 +0800

    [done] add: ColumnTextToNumber func

--- excel/excel.go
+++ excel/excel.go
@@ -4,7 +4,25 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-// ColumnIndexToText 列索引转文字
-func (r ExcelWriter) ColumnIndexToText(columnIndex int) (string, error) {
-	return excelize.ColumnNumberToName(columnIndex)
+// ColumnNumberToText 列索引转文字
+func ColumnNumberToText(columnNumber int) (string, error) {
+	return excelize.ColumnNumberToName(columnNumber)
+}
+
+// ColumnTextToNumber 列文字转索引
+func ColumnTextToNumber(columnText string) int {
+	result := 0
+	for i, char := range columnText {
+		result += (int(char - 'A' + 1)) * pow(26, len(columnText)-i-1)
+	}
+	return result
+}
+
+// pow 是一个简单的幂函数计算，用于26进制转换
+func pow(base, exponent int) int {
+	result := 1
+	for i := 0; i < exponent; i++ {
+		result *= base
+	}
+	return result
 }
